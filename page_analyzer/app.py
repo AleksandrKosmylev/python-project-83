28d0e03acf80477c08412b77be42b465d496969c
AleksandrKosmylev <alex.kosmylev@gmail.com>
2023-06-11 13:38:04 +0300

    fix app

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -2,15 +2,12 @@ from flask import Flask, render_template
 
 
 app = Flask(__name__)
-"""
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
-"""
+
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+    return 'Hello, World!'
+
 
 if __name__ == '__main__':
     hello_world()
