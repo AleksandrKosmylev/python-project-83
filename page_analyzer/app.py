66c181fd14ff6c78084f7af504166eaf6d15cd1c
AleksandrKosmylev <alex.kosmylev@gmail.com>
2023-06-16 11:35:27 +0300

    add connect to database

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -18,13 +18,13 @@ def index():
 #@app.route("/", methods=['POST'])
 @app.post("/urls")
 def site_check():
-    fill  = request.form['url']
+    #fill  = request.form['url']
     # return redirect(url_for('success', name=user))
     #return render_template('urls/index.html' )
     #return fill
     try:
         conn = psycopg2.connect(dbname='urls', user='postgres', password='paralich666', host='host')
-        return "success"
+        return conn"
     except:
         print('Can`t establish connection to database')
 
