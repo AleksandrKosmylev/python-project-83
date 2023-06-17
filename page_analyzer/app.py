48fd95267afa533e16a6a38a9bf8c6b7cf0b1377
AleksandrKosmylev <alex.kosmylev@gmail.com>
2023-06-17 17:34:58 +0300

    add connect to database

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,38 +1,46 @@
 import psycopg2
+from psycopg2.extras import NamedTupleCursor
 from flask import (
     Flask,
-    render_template,
-    request,
-    redirect,
+    render_template
 )
 
 
 app = Flask(__name__)
 
-export DATABASE_URL=postgresql://postgresql:paralich666@localhost:5432/urls
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
-#@app.route("/", methods=['POST'])
 @app.post("/urls")
 def site_check():
-    #fill  = request.form['url']
+    # fill  = request.form['url']
     # return redirect(url_for('success', name=user))
-    #return render_template('urls/index.html' )
-    #return fill
-
+    # return render_template('urls/index.html' )
+    # return fill
     conn = psycopg2.connect(host="127.0.0.1",
-                            dbname="urls",
+                            dbname="postgres",
                             user="postgres",
                             password="paralich666")
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM suppliers LIMIT 10')
-    cursor.fetchall()
-    cursor.close()
-    conn.close()
+
+    """
+    with conn.cursor(cursor_factory=NamedTupleCursor) as curs:
+        curs.execute('SELECT * FROM urls')
+        check = curs.fetchall()
+        return f'{check}'
+    """
+    with conn.cursor(cursor_factory=NamedTupleCursor) as curs:
+        curs.execute("""
+        INSERT INTO urls (name, created_at)
+        VALUES(%s, %s);
+        """,
+        ('data!!', 'now'))
+        curs.execute('SELECT * FROM urls')
+        check = curs.fetchall()
+        return f'{check}'
+        conn.close()
 
 
 if __name__ == '__main__':
