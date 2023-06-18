4abf27ab374cd1885956b824cb3e6bdd7f78a3af
AleksandrKosmylev <alex.kosmylev@gmail.com>
2023-06-18 18:49:16 +0300

    change host '127.0.0.1' to "0.0.0.0"

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -37,7 +37,7 @@ def site_check():
         INSERT INTO urls (name, created_at)
         VALUES(%s, %s);
         """,
-        (fill, 'now'))
+        ("data@", 'now'))
         curs.execute('SELECT * FROM urls')
         check = curs.fetchall()
         return f'{check}'
@@ -45,5 +45,5 @@ def site_check():
 
 
 if __name__ == '__main__':
-    #index(host='0.0.0.0', port=8080)
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    index()
+    #app.run(host='0.0.0.0', port=8000, debug=True)
