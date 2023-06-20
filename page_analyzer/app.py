c07f484c48a3884669a031f341f3e7964dc1809f
AleksandrKosmylev <alex.kosmylev@gmail.com>
2023-06-20 22:00:47 +0300

    connect to railway database

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -21,18 +21,7 @@ def index():
 @app.post("/urls")
 def site_check():
     fill = request.form['url']
-    # return redirect(url_for('success', name=user))
-    # return render_template('urls/index.html' )
-    # return fill
     conn = psycopg2.connect('postgresql://postgres:im3Dc5o5ENPMrEbQaU0x@containers-us-west-24.railway.app:7634/railway')
-    
-
-    """
-    with conn.cursor(cursor_factory=NamedTupleCursor) as curs:
-        curs.execute('SELECT * FROM urls')
-        check = curs.fetchall()
-        return f'{check}'
-    """
     with conn.cursor(cursor_factory=NamedTupleCursor) as curs:
         curs.execute("""
         INSERT INTO urls (name, created_at)
@@ -47,4 +36,3 @@ def site_check():
 
 if __name__ == '__main__':
     index()
-    #app.run(host='0.0.0.0', port=8000, debug=True)
