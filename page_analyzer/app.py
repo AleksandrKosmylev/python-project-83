65a6173b005476754ece5d4d061ea7741eff388b
AleksandrKosmylev <alex.kosmylev@gmail.com>
2023-07-01 15:24:44 +0300

    add url_database

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -33,22 +33,7 @@ def index():
 
 @app.post("/urls")
 def site_check():
-    fill = request.form['url']
-    errors = validate(fill)
-    if errors:
-        return "Error", 422
-    
-    conn = psycopg2.connect('postgresql://postgres:2CySg27TBeJhsb8jZ6IM@containers-us-west-57.railway.app:6548/railway')
-    with conn.cursor(cursor_factory=NamedTupleCursor) as curs:
-        curs.execute("""
-        INSERT INTO urls (name, created_at)
-        VALUES(%s, %s);
-        """,
-        (fill, 'now'))
-        curs.execute('SELECT * FROM urls')
-        check = curs.fetchall()
-        return f'{check}'
-        conn.close()
+    return "smt"
 
 
 if __name__ == '__main__':
