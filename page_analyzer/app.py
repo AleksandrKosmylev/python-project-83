c372851b19a152b5cb8ec491f3e6b2728e7cd04f
AleksandrKosmylev <alex.kosmylev@gmail.com>
2023-07-04 18:49:20 +0300

    add env file

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -37,16 +37,15 @@ def site_check():
 
     conn = psycopg2.connect(DATABASE_URL)
     with conn.cursor() as curs:
-        curs.execute(
-        """
+        curs.execute("""
         INSERT INTO urls (name, created_at)
         VALUES(%s, %s);
         """,
-        (fill, 'now')
-        )
+        ("data@", 'now'))
         curs.execute('SELECT * FROM urls')
         check = curs.fetchall()
         return f'{check}'
+        conn.close()
 
 
 if __name__ == '__main__':
