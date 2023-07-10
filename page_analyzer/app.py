d94d980fc829bdbb2f3b51f31f5ad9020b33b4e7
AleksandrKosmylev <alex.kosmylev@gmail.com>
2023-07-10 23:29:56 +0300

    add  def analyze

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -72,24 +72,16 @@ def site_check():
 def analyze(id):
     conn = psycopg2.connect(DATABASE_URL)
     urls_id = int(id)
-    
     with conn.cursor(cursor_factory=NamedTupleCursor) as curs:
         curs.execute(
+            # "SELECT name FROM urls WHERE id =  %s", (urls_id, )
             "SELECT  * FROM urls"
         )
-        checkr_url = curs.fetchall()
-        print(checkr_url[0][0])
-        
-    with conn.cursor(cursor_factory=NamedTupleCursor) as curs:
-        curs.execute(
-            "SELECT name FROM urls WHERE id =  %s", (urls_id, )
-            #"SELECT  * FROM urls"
-        )
         result_url = curs.fetchall()
-    # (url_id, name, created_at) = result_url
+        (url_id, name, created_at) = result_url
         print("id=", type(id), id)
         print("urls_id=", type(urls_id), urls_id)
-        print(result_url)
+        print(url_id, name, created_at)
         return result_url
 
 
